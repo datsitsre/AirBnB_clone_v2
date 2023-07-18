4d0dcc10d9fa3eb877253804562ce020985045a6
vagrant <datsitsre>
2023-07-18 13:31:50 +0000

    Task 2

---
+++ 1-pack_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Fabric  to create an archive """
+
+# import libraries
+import os
+from datetime import datetime
+from fabric.api import local, runs_once
+
+
+@runs_once
+def do_pack():
+    """ the Archive code """
+    if no os.path.isdir("versions"):
+        os.mkdir("versions")
+    current_time = datetime.now()
+    results = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        current_time.year,
+        current_time.month,
+        current_time.day,
+        current_time.hour,
+        current_time.minute,
+        current_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
