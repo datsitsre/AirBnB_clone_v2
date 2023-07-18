e041a67e25f44dd8c86447b4a30fb17d8365d3ea
vagrant <datsitsre>
2023-07-18 14:56:48 +0000

    Task 3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,33 +16,20 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
+     if os.path.exists("archive_path"):
+        file_name = os.path.basename(archive_apth)
+        dir_name = file_name.replace(".tgz", "")
+        dir_path = "/data/web_static/releases/{}/".format(dir_name)
+        try:
+            put(archive_path, "/tmp/{}".format(file_name))
+            run("mkdir -p {}".format(dir_path))
+            run("tar -xzf /tmp/{} -C {}".format(file_name, dir_path))
+            run("rm -rf /tmp/{}".format(file_name))
+            run("mv {}web_static/* {}".format(dir_path, dir_path))
+            run("rm -rf {}web_static".format(dir_path))
+            run("rm -rf /data/web_static/current")
+            run("ln -s {} /data/web_static/current".format(dir_path))
+            print('New version deployed!')
+        except Exception:
+            return False
+        return True
