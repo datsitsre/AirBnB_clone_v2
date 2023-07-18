7a460cf78812a6d2effe2b21f4362a4bb1a3b544
vagrant <datsitsre>
2023-07-18 15:22:09 +0000

    Advance task

---
+++ 100-clean_web_static.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+""" Fabric  to create an archive """
+import os
+from fabric.api import env, put, run
+from datetime import datetime
+from fabric.api import local, runs_once
+
+
+env.hosts = ["104.196.168.90", "35.196.46.172"]
+
+
+@runs_once
+def do_pack():
+    """ the Archive code """
+    current_time = datetime.utcnow()
+    if os.path.isdir("web_static") is False:
+        local("mkdir -p web_static")
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(current_time.year,
+                                                         current_time.month,
+                                                         current_time.day,
+                                                         current_time.hour,
+                                                         current_time.minute,
+                                                         current_time.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
+
+
+def do_deploy(archive_path):
+    """Archive to a web server.
+
+    Args:
+        Archive_path : The path of the archive to distribute.
+    Returns:
+        Return archive or False
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
+
+
+def deploy():
+    """
+        Archives and deploys files to the host servers.
+    """
+    archive_path = do_pack()
+    return do_deploy(archive_path) if archive_path else False
+
+
+def do_clean(number=0):
+    """Deletes archives of the static files that are out-of-date
+    Args:
+        number: The number of archives to keep.
+    """
+    archives = os.listdir('versions/')
+    archives.sort(reverse=True)
+    start = int(number)
+    if not start:
+        start += 1
+    if start < len(archives):
+        archives = archives[start:]
+    else:
+        archives = []
+    for archive in archives:
+        os.unlink('versions/{}'.format(archive))
+    cmd_parts = [
+        "rm -rf $(",
+        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
+        " '/data/web_static/releases/web_static_.*'",
+        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
+    ]
