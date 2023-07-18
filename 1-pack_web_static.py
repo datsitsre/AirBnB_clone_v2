156115e2cd89f5ed8271f8c03889cec4b5f91fa5
vagrant <datsitsre>
2023-07-18 13:48:39 +0000

    Task 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,22 +10,16 @@ from fabric.api import local, runs_once
 @runs_once
 def do_pack():
     """ the Archive code """
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    current_time = datetime.now()
-    results = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        current_time.year,
-        current_time.month,
-        current_time.day,
-        current_time.hour,
-        current_time.minute,
-        current_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+    current_time = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(current_time.year,
+                                                         current_time.month,
+                                                         current_time.day,
+                                                         current_time.hour,
+                                                         current_time.minute,
+                                                         current_time.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
