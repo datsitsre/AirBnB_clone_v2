b1b4a49d86381dc545a8f3bdeef5a9e8f64e4959
vagrant <datsitsre>
2023-07-18 14:23:05 +0000

    Task 3

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Fabric  to create an archive """
+
+# import libraries
+import os
+from datetime import datetime
+from fabric.api import local, runs_once
+
+
+@runs_once
+def do_pack():
+    """ the Archive code """
+    current_time = datetime.utcnow()
+    if os.path.isdir("web_static") is False:
+        local("mkdir -p web_static")
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(current_time.year,
+                                                         current_time.month,
+                                                         current_time.day,
+                                                         current_time.hour,
+                                                         current_time.minute,
+                                                         current_time.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
+
+
+def do_deplay(archive_path):
+    """ Deplay static files on the host """
+    if os.path.exists("archive_path"):
+        file_name = os.path.basename(archive_apth)
+        dir_name = file_name.replace(".tgz", "")
+        dir_path = "/data/web_static/releases/{}/".format(dir_name)
+        try:
+            put(archive_path, "/tmp/{}".format(file_name))
+            run("mkdir -p {}".format(dir_path))
+            run("tar -xzf /tmp/{} -C {}".format(file_name, dir_path))
+            run("rm -rf /tmp/{}".format(file_name))
+            run("mv {}web_static/* {}".format(dir_path, dir_path))
+            run("rm -rf {}web_static".format(dir_path))
+            run("rm -rf /data/web_static/current")
+            run("ln -s {} /data/web_static/current".format(dir_path))
+            print('New version deployed!')
+        except Exception:
+            return False
+        return False
