96f8d9d0f6c7ac4df9d50c65249bf1f88b2792da
vagrant <datsitsre>
2023-07-20 09:59:23 +0000

    Task 1

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Flask applications to listen on port 50000
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hbnb_hello():
+    ''' print hello HBNB '''
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port='5000')
+
