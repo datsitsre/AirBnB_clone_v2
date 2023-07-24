43375831754188288ca69405409ad89f2e41d6f3
vagrant <datsitsre>
2023-07-24 10:57:45 +0000

    Read me file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hbnb_hello():
     ''' print hello HBNB '''
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
