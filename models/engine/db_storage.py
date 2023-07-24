8f84bc13bf66f086d48792c43598f823b38a92d2
vagrant <datsitsre>
2023-07-24 15:57:05 +0000

    Task 4 5

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage database storage for hbnb clone"""
 import os
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine
 import urllib.parse
 
