0b31dc0d5f5a22c9a5fdca9dd831d999bd02dc75
vagrant <datsitsre>
2023-07-24 16:01:22 +0000

    Task 4 5

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,6 @@
 """This module instantiates an object of class FileStorage"""
 import os
 
-from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 
 storage = DBStorage() if os.getenv(
