4f0ad1417aea4da3274f445697ac61eef4b3bd81
vagrant <datsitsre>
2023-07-24 10:54:40 +0000

    Read me file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,5 +12,5 @@ def hbnb_hello():
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port='5000')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
